88e839470ef795f8e62e97516a8bf3c590001ad9
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-08 07:30:04 +0000

    Load content dynamically

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './styles.css';
 import picture from './assets/restaurant.jpg';
+import { loadPage } from './pageLoad.js';
 
 console.log('hrllow');
+loadPage();
---
+++ src/pageLoad.js
@@ -0,0 +1,24 @@
+import restaurantPicture from './assets/restaurant.jpg';
+
+const loadPage = function () {
+    const contentDiv = document.querySelector("#content");
+
+    const img = document.createElement("img");
+    img.src = restaurantPicture;
+    img.alt = "Restaurant image";
+    contentDiv.append(img);
+
+    const h1 = document.createElement("h1");
+    h1.textContent = "Welcome to Simple Restaurant";
+    contentDiv.append(h1);
+
+    const p = document.createElement("p");
+    p.textContent = "At Simple Restaurant, we serve heartwarming meals made" +
+        "with love and fresh ingredients. Whether you're here for a quick bite or a " +
+        "cozy dinner, you'll always be greeted with a smile and a plate" +
+        "full of flavor."
+
+    contentDiv.append(p);
+}
+
+export {loadPage};
