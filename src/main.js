e0a277cebb14bc406d7b0fea8fe52304d01b0911
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-08 08:01:30 +0000

    Make the buttons work

--- src/main.js
+++ src/main.js
@@ -5,7 +5,19 @@ import { loadMenu } from './menu.js';
 import { loadAbout } from './about.js';
 
 console.log('hrllow');
-// loadHome();
-// loadMenu();
 
-loadAbout();
+loadHome();
+
+const buttons = document.querySelectorAll("button");
+const div = document.querySelector("#content");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        div.textContent = null;
+        switch(event.target.id) {
+            case 'home': loadHome();break;
+            case 'menu': loadMenu();break;
+            case 'about': loadAbout();break;
+        }
+    })
+})
